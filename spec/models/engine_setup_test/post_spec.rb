a150c849bea94111e966879161487eca8b012710
Caden Lovelace <caden@herostrat.us>
2014-10-22 11:40:05 +0100

    Add test. Job done!

--- spec/models/engine_setup_test/post_spec.rb
+++ spec/models/engine_setup_test/post_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 module EngineSetupTest
   RSpec.describe Post, :type => :model do
-    pending "add some examples to (or delete) #{__FILE__}"
+
+    it "has a valid factory" do
+      expect(FactoryGirl.build(:post)).to be_valid
+    end
+
   end
 end
